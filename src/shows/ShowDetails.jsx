76a1e8ef890f91333b250901c13f4255f3fbf032
Zachary Devine <zach.devine17@gmail.com>
2025-06-06 13:39:04 -0700

    commit

--- src/shows/ShowDetails.jsx
+++ src/shows/ShowDetails.jsx
@@ -1,6 +1,30 @@
+import { useState } from "react";
 import "./shows.css";
+import EpisodeDetails from "../episodes/EpisodeDetails";
+import EpisodeList from "../episodes/EpisodeList";
 
 /** Allows users to browse through the episodes of the given show */
-export default function ShowDetails() {
-  return <div className="show-details"></div>;
+export default function ShowDetails({show}) {
+  const [selectedEp, setSelectedEp] = useState(null);
+  if(!show){
+    return(
+      <>
+        <h2>Show Details</h2>
+        <p>Please select a show to see its available episodes</p>
+      </>
+    );
+  }
+  return(
+    <>
+      <EpisodeList 
+        name={show.name}
+        episodes={show.episodes}
+        selectedEpisode={selectedEp}
+        setSelectedEpisode={setSelectedEp}
+      />
+      <EpisodeDetails 
+        episode={selectedEp}
+      />
+    </>
+  );
 }
